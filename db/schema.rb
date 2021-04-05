69113c944dbde125cdbae45c8baf8536c40ded67
sergiorock <lopez.sergio.n@gmail.com>
2021-04-05 19:07:23 -0300

    add Hamlit to manage templates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_181048) do
+ActiveRecord::Schema.define(version: 2021_03_22_215821) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "description"
+    t.string "isbn"
+    t.integer "pages_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
